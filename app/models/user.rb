6b5108ec1502b571173aeb2ce97561eeacc2f951
Laven Morrow <lmorrow13@live.com>
2018-02-28 16:46:29 -0700

    integrity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,24 @@
 class User < ApplicationRecord
-	has_many :posts
-	has_many :comments
+	has_many :posts, dependent: :destroy
+	has_many :comments, dependent: :destroy
+
+	validates :email, presence: true, uniqueness: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :password, presence: true, length: {minimum: 6}
+
+	scope :last_3_days, -> {
+		User.where('created_at between ? and ?', DateTime.now - 3.days, Time.now)
+	}
+
+	scope :no_bio, -> {
+		where("bio IS ? OR bio IS ?", nil, '')
+	}
+
+	scope :authenticate_login,  -> (email, password) {
+		user = User.find_by(email: email)
+		if user && user.password == password
+			user
+		end
+	}
 end
